347f3453f84d1e40f4bf702e6f37bc3a015cc72a
asbudik <asbudik@live.com>
2014-08-12 20:51:21 -0700

    complete min reqs homework tuesday week 6

--- scrapper.rb
+++ scrapper.rb
@@ -6,31 +6,46 @@ require 'open-uri'
 
 def filter_links(rows, regex)
   # takes in rows and returns uses
-  # regex to only return links 
+  # regex to only return links
   # that have "pup", "puppy", or "dog"
   # keywords
+
+  if rows.content.match(regex)
+    matches = rows
+    puts matches.text
+    puts "*" * 60
+  end
 end
 
 def get_todays_rows(doc, date_str)
   #  1.) open chrome console to look in inside p.row to see
   #  if there is some internal date related content
-
   #  2.) figure out the class that you'll need to select the
   #   date from a row
 
+  doc.each do |el|
+    if el.css(".date").text.match(/Aug 12/)
+      filter_links(el, /Dog|dog|puppies|Puppies|Puppy|puppy|Pup|pup/)
+    end
+  end
 end
 
 def get_page_results
-  url = "http://sfbay.craigslist.org/sfc/pet/"
-  Nokogiri::HTML(open(url))
+  url = "today.html"
+  @doc = Nokogiri::HTML(open(url))
+  rows = @doc.css(".row")
+  date = @doc.css(".date").text
+
+  get_todays_rows(rows, date)
 end
 
 def search(date_str)
-  get_page_results
+  get_page_results()
 end
 
-# want to learn more about 
+# want to learn more about
 # Time in ruby??
 #   http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/Date.html#strftime-method
-today = Time.now.strftime("%b %d")
+# today = Time.now.strftime("%b %d")
+today = "Aug 12"
 search(today)
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,6 +1,6 @@
 require "./scrapper"
 
-describe "scrapper" do 
+describe "scrapper" do
 
   before(:each) do
     @doc = Nokogiri::HTML(open("today.html"))
@@ -10,13 +10,24 @@ describe "scrapper" do
   describe "filter_links" do
     it "should here is an example of using the befor each data" do
       rows = @doc.css(".row")
-      rows.each do |row|
-        puts row.content
-      end
-      expect("your attention").to eql("this test to be erased or edited")
+      # puts rows[0].content
+      # rows.each do |row|
+      #   puts row.content
+      # end
+      expect(filter_links(rows[0].content, /Albino|dog|puppies/)).to be(true)
     end
   end
 
   describe "get_todays_rows" do
+    it "should return today's rows" do
+      count = 0
+      @doc.css(".date").each do |el|
+        if el.content = "Aug 12"
+          puts el
+          puts count += 1
+        end
+      end
+      expect(get_todays_rows(@doc, "Aug 12")).to eql(100)
+    end
   end
 end
